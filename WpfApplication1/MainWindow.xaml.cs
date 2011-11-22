6928eca4d5720360763f00308ef1ba1824f33f20
unknown <cblades@VAT-CBLADES.cb.careerbuilder.com>
2011-11-21 23:06:49 -0500

    initial commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StepManager StepMan;
+
+        private void SetupStepManager()
+        {
+            StepMan = new StepManager()
+            .AddStep(lblStep1)
+            .AddStep(lblStep2)
+            .AddStep(lblStep3)
+            .AddStep(lblStep4)
+            .AddStep(lblStep5);
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DoubleClickStep1(object sender, MouseButtonEventArgs e)
+        {
+            StepMan.ChangeStep(1);
+        }
+
+        private void DoubleClickStep2(object sender, MouseButtonEventArgs e)
+        {
+            StepMan.ChangeStep(2);
+        }
+
+        private void DoubleClickStep3(object sender, MouseButtonEventArgs e)
+        {
+            StepMan.ChangeStep(3);
+        }
+
+        private void DoubleClickStep4(object sender, MouseButtonEventArgs e)
+        {
+            StepMan.ChangeStep(4);
+        }
+
+        private void DoubleClickStep5(object sender, MouseButtonEventArgs e)
+        {
+            StepMan.ChangeStep(5);
+        }
+
+
+
+        
+    }
+}
---
+++ WpfApplication1/StepManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApplication1
+{
+    internal class StepManager
+    {
+        private IList<Label> Steps { get; set; }
+
+        public int CurrentStep
+        {
+            public get;
+            private set;
+        }
+
+        public int Max
+        {
+            get
+            {
+                return Steps.Count;
+            }
+        }
+
+        public StepManager(IEnumerable<Label> steps)
+        {
+            Steps = steps.ToList();
+        }
+
+        public StepManager()
+        {
+            Steps = new List<Label>();
+        }
+
+        public StepManager AddStep(Label step)
+        {
+            if (step != null)
+                Steps.Add(step);
+
+            return this;
+        }
+
+        public void ChangeStep(int stepNum)
+        {
+            bool stepNumIsInvalid = stepNum > Max || stepNum < 1;
+            if (stepNumIsInvalid)
+                return;
+
+            CurrentStep = stepNum;
+
+            int stepIndex = stepNum - 1;
+            var lblCurrentStep = Steps[stepIndex];
+            lblCurrentStep.FontWeight = FontWeights.ExtraBold;
+
+            Steps.Where(step => step != lblCurrentStep).ToList().ForEach(step => step.FontWeight = FontWeights.Normal);
+        }
+
+        public void AdvanceStep()
+        {
+            if (CurrentStep >= Max)
+                return;
+
+            ChangeStep(CurrentStep + 1);
+        }
+
+        public void RetreatStep()
+        {
+            if (CurrentStep <= 1)
+                return;
+
+            ChangeStep(CurrentStep - 1);
+        }
+    }
+}
