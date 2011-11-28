f8c117082e16980cd2c4ad81ace30ee160ffaadf
Christian Blades <christian.blades@docblades.com>
2011-11-28 16:08:51 -0500

    Working Wizard

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -19,16 +19,36 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private StepManager StepMan;
+        private StepAndWizardManager StepMan;
 
         private void SetupStepManager()
         {
-            StepMan = new StepManager()
-            .AddStep(lblStep1)
-            .AddStep(lblStep2)
-            .AddStep(lblStep3)
-            .AddStep(lblStep4)
-            .AddStep(lblStep5);
+            StepMan = new StepAndWizardManager()
+            .AddStep(new WizardStep()
+            {
+                StepLabel = lblStep1,
+                WizardControl = wizard11
+            })
+            .AddStep(new WizardStep()
+            {
+                StepLabel = lblStep2,
+                WizardControl = wizard21
+            })
+            .AddStep(new WizardStep()
+            {
+                StepLabel = lblStep3,
+                WizardControl = wizard31
+            })
+            .AddStep(new WizardStep()
+            {
+                StepLabel = lblStep4,
+                WizardControl = wizard41
+            })
+            .AddStep(new WizardStep()
+            {
+                StepLabel = lblStep5,
+                WizardControl = wizard51
+            });
         }
 
         public MainWindow()
@@ -40,30 +60,68 @@ namespace WpfApplication1
         private void DoubleClickStep1(object sender, MouseButtonEventArgs e)
         {
             StepMan.ChangeStep(1);
+            ManageStepChange();
         }
 
         private void DoubleClickStep2(object sender, MouseButtonEventArgs e)
         {
             StepMan.ChangeStep(2);
+            ManageStepChange();
         }
 
         private void DoubleClickStep3(object sender, MouseButtonEventArgs e)
         {
             StepMan.ChangeStep(3);
+            ManageStepChange();
         }
 
         private void DoubleClickStep4(object sender, MouseButtonEventArgs e)
         {
             StepMan.ChangeStep(4);
+            ManageStepChange();
         }
 
         private void DoubleClickStep5(object sender, MouseButtonEventArgs e)
         {
             StepMan.ChangeStep(5);
+            ManageStepChange();
         }
 
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
 
+        private void ManageStepChange()
+        {
+            if (StepMan.CurrentStep == StepMan.MaxSteps)
+            {
+                button1.IsEnabled = true;
+                button2.IsEnabled = false;
+            }
+            else
+            {
+                button1.IsEnabled = false;
+                button2.IsEnabled = true;
+            }        
 
+            if (StepMan.CurrentStep == 1)
+                button3.IsEnabled = false;
+            else
+                button3.IsEnabled = true;            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            StepMan.AdvanceStep();
+            ManageStepChange();
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            StepMan.RetreateStep();
+            ManageStepChange();
+        }
         
     }
 }
---
+++ WpfApplication1/StepAndWizardManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    internal class StepAndWizardManager
+    {
+        private IList<WizardStep> StepsAndWizards { get; set; }
+        private const int DEFAULTFONTSIZE = 12;
+        private const int ACTIVEFONTSIZE = 16;
+
+        public StepAndWizardManager()
+        {
+            CurrentStep = 1;
+            StepsAndWizards = new List<WizardStep>();
+        }
+
+        public int MaxSteps
+        {
+            get 
+            {
+                return StepsAndWizards.Count;
+            }
+        }
+
+        public int CurrentStep { get; set; }
+
+        public StepAndWizardManager AddStep(WizardStep step)
+        {
+            StepsAndWizards.Add(step);
+            return this;
+        }
+
+        public void ChangeStep(int stepNum)
+        {
+            CurrentStep = stepNum;
+            int stepIndex = stepNum - 1;
+
+            foreach (var item in StepsAndWizards.Where(step=>step.StepLabel != StepsAndWizards[stepIndex].StepLabel))
+            {
+                item.StepLabel.FontSize = DEFAULTFONTSIZE;
+                item.StepLabel.FontWeight = FontWeights.Normal;
+                item.WizardControl.Visibility = Visibility.Hidden;
+            }
+
+            var activeItem = StepsAndWizards[stepIndex];
+            activeItem.StepLabel.FontSize = ACTIVEFONTSIZE;
+            activeItem.StepLabel.FontWeight = FontWeights.Bold;
+            activeItem.WizardControl.Visibility = Visibility.Visible;            
+        }
+
+        public void AdvanceStep()
+        {
+            if (CurrentStep >= MaxSteps)
+                return;
+            ChangeStep(CurrentStep + 1);
+        }
+
+        public void RetreateStep()
+        {
+            if (CurrentStep <= 1)
+                return;
+            ChangeStep(CurrentStep - 1);
+        }
+    }
+}
